00b7b4f46bf7226fd989325ff9f49509cdaf2d50
Andrici <andrici.andu@gmail.com>
2024-08-06 10:08:39 +0300

    Test potential fix for css modules not loading properly

--- webpack.config.js
+++ webpack.config.js
@@ -29,16 +29,23 @@ module.exports = {
         use: "babel-loader",
       },
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: [
           "style-loader",
           {
-            loader: 'css-loader', 
+            loader: "css-loader",
             options: {
-              modules: true
-            }
-          }
+              importLoaders: 1,
+              modules: true,
+            },
+          },
         ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
       },
     ],
   },
